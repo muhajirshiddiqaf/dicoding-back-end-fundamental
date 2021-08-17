46b593137a6b3db2002f91f9727281324cbcbeb1
bagas31 <faruq.al.ms@gmail.com>
2021-08-17 11:58:44 +0700

    update

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const Hapi = require('@hapi/hapi');
+
+
+/*Notes Service Config*/
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService') 
+/*End Notes*/
+
+/*Notes validator*/
+const NotesValidator = require('./validator/notes');
+/*End Notes validator*/
+
+const init = async () => {
+  const notesService = new NotesService();
+
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: notes,
+    options:{
+      service: notesService,
+      validator: NotesValidator,
+    }
+  })
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
